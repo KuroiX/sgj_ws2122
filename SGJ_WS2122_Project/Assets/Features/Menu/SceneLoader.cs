0d2422bb3937356ceb7accb0ae7a02a0bb6cb8c0
Kuroi <geheeb@in.tum.de>
2021-12-12 02:45:11 +0100

    Final (?) changes

--- SGJ_WS2122_Project/Assets/Features/Menu/SceneLoader.cs
+++ SGJ_WS2122_Project/Assets/Features/Menu/SceneLoader.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
-{
+{ 
     private void OnTriggerEnter(Collider other)
     {
         LoadNextScene();
@@ -13,8 +13,10 @@ public class SceneLoader : MonoBehaviour
     public void LoadNextScene()
     {
         int newIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        //Debug.Log(newIndex);
+        //Debug.Log(SceneManager.sceneCount);
 
-        if (newIndex >= SceneManager.sceneCount)
+        if (newIndex >= SceneManager.sceneCountInBuildSettings)
         {
             Application.Quit();
             Debug.Log("Quit");
